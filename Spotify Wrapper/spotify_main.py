9a15064ef48303b89c2b682f80c3907418b85e66
michalspano <speko.spano@gmail.com>
2021-06-25 21:33:45 +0200

    25/06/2021; Spotify Wrapper final, Flask web-provider

--- Spotify Wrapper/spotify_main.py
+++ Spotify Wrapper/spotify_main.py
@@ -3,6 +3,7 @@ import spotipy
 import discord
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
+from web import keep_alive
 
 api = {"discord_client": discord.Client(),
        "spotify_client": spotipy.Spotify(auth_manager=SpotifyClientCredentials())}
@@ -21,14 +22,37 @@ class Spotify:
 
     @staticmethod
     def embed_music_data(data):
-        embed_title, embed_image, embed_url = data[0], data[1], data[2]
-        embed_message = discord.Embed(title=embed_title, url=embed_url)
-        embed_message.set_image(url=embed_image)
+        embed_data = {"embed_title": data[0],
+                      "embed_image": data[1],
+                      "embed_url": data[2]}
+        embed_message = discord.Embed(title=embed_data["embed_title"], url=embed_data["embed_url"])
+        embed_message.set_image(url=embed_data["embed_image"])
+        return embed_message
+
+
+class HelpCommands:
+    def __init__(self, path):
+        self.path = path
+
+    #  Returns the !help command content with discord.Embed()
+    def embed_help_commands(self):
+        help_commands_desc = str()
+        with open(self.path, "r") as help_commands:
+            for line in help_commands.readlines():
+                help_commands_desc += line
+        embed_message = discord.Embed(title="!help commands",
+                                      description=help_commands_desc,
+                                      url=str(os.environ["GITHUB_LINK"]),
+                                      color=discord.Color.dark_green())
+        embed_message.set_author(name=api["discord_client"].user.display_name,
+                                 icon_url=api["discord_client"].user.avatar_url)
         return embed_message
 
 
 @api["discord_client"].event
 async def on_ready():  # Log-Status BOT
+    await api["discord_client"].change_presence(activity=discord.Activity(type=discord.ActivityType.listening,
+                                                                          name="a song | !help to start"))  # Custom Bot status
     print("BOT Status: ON", api["discord_client"].user)
 
 
@@ -40,7 +64,7 @@ async def on_message(message):
 
     #  Returns a musical thread, syntax: '!s' - a random song,
     #  '!s/genre' - a random song from a specified genre
-    if message.channel.name == "music" and message.content.startswith("!s"):
+    if message.content.startswith("!s"):
         if message.content.startswith("!s"):
             msg = message.content.split("/")
 
@@ -48,7 +72,7 @@ async def on_message(message):
             if len(msg) > 1:
                 genre = msg[1]
             else:  # Default genre selection
-                genre = r.choice(["Pop", "Hip-Hop", "Rap", "Ambient"])
+                genre = r.choice(["Pop", "Hip-Hop", "Rap", "Ambient", "EDM"])
 
             #  Randomized arr[song_title, song_cover, song_url]
             final_result = Spotify(artist=f"{genre}").parse_music_data()
@@ -56,7 +80,12 @@ async def on_message(message):
             embed_object = Spotify.embed_music_data(final_result)
 
             await message.channel.send(embed=embed_object)  # Embed msg
-            await message.channel.send(message_object_default_embed)  # Non-embed msg
+            await message.channel.send(message_object_default_embed)  # Non-embed msg / in-built 'Spotify Preview'
+
+    elif message.content.startswith("!help"):
+        returned_embed = HelpCommands(path="help_command.txt").embed_help_commands()
+        await message.channel.send(embed=returned_embed)
 
 
+keep_alive()  # Online WebProvider through Flask
 api["discord_client"].run(os.environ["TOKEN"])
