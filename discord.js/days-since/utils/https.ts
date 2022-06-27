d03512e569bafcbc709c5e23ea01e083ed96acd9
michalspano <speko.spano@gmail.com>
2022-06-27 16:21:02 +0200

    Add `utils` of `days-since`

---
+++ discord.js/days-since/utils/https.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const port: number = 3000;
+const app: express.Application = express();
+
+app.get('/', (req, res) => res.send('Bot is ON!'));
+app.listen(port, () => console.log(`Running on ${port}!`));
